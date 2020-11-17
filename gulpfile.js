e42857492b4195ca1acc0355fa25e6a54e5ad749
Vyacheslav Golovanov <privetslavka@gmail.com>
2020-11-17 20:59:02 +0300

    Добавил gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,34 +18,34 @@ const styles = () => {
     ]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
-    .pipe(sync.stream());
+    // .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
 // Server
 
-const server = (done) => {
-  sync.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-}
+// const server = (done) => {
+//   sync.init({
+//     server: {
+//       baseDir: 'source'
+//     },
+//     cors: true,
+//     notify: false,
+//     ui: false,
+//   });
+//   done();
+// }
 
-exports.server = server;
+// exports.server = server;
 
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html")/*.on("change", sync.reload)*/;
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  styles /*server*/, watcher
 );
